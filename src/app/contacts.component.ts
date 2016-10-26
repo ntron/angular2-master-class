de2988025f1cf56cf5dbd9f8bc53473fdc270fd8
Maik Siegert <maik.siegert@gmail.com>
2016-10-26 11:05:34 +0200

    * exercise 1 and 2

--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { Contact } from './models/contact'
+
 
 @Component({
   selector: 'trm-contacts-app',
   templateUrl: 'contacts.component.html',
   styleUrls: ['contacts.component.css']
 })
+
 export class ContactsAppComponent {
   title = 'Angular 2 Master Class setup works!';
+  contact: Contact = {
+    id: 7,
+    name: 'Diana Ellis',
+    email: '',
+    phone: '',
+    birthday: '',
+    website: '',
+    image: '/assets/images/7.jpg',
+    address: {
+      street: '6554 park lane',
+      zip: '43378',
+      city: 'Rush',
+      country: 'United States'
+    }
+  }
 }
